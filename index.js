4615ba2205fb56a82259448a169ad97c78813e15
EJohn <dev.ejohnpaguntalan@gmail.com>
2025-03-16 01:37:44 +0800

    implement on dismiss event when clicking outside the popover

--- index.js
+++ index.js
@@ -1,37 +1,19 @@
-// const shareButton = document.querySelector(".share-button");
-// const author = document.querySelector(".author");
-// const shareOptions = document.querySelector(".share-options");
-
-// const openToast = (message) => {
-//   shareButton.addEventListener("click", () => {
-//     console.log(message);
-//     shareButton.classList.toggle("active");
-
-//     if (shareButton.classList.contains("active")) {
-//       author.style.opacity = "0";
-//       author.style.visibility = "hidden";
-//       author.style.pointerEvents = "none";
-
-//       shareOptions.style.opacity = "1";
-//       shareOptions.style.visibility = "visible";
-//       shareOptions.style.pointerEvents = "auto";
-//     } else {
-//       author.style.opacity = "1";
-//       author.style.visibility = "visible";
-//       author.style.pointerEvents = "auto";
-
-//       shareOptions.style.opacity = "0";
-//       shareOptions.style.visibility = "hidden";
-//       shareOptions.style.pointerEvents = "none";
-//     }
-//   });
-// };
-
-// openToast("Open share-to-other-social-media-sites toast");
-
 const shareButton = document.querySelector(".share-button");
+const shareOptions = document.querySelector(".share-options");
 
 shareButton.addEventListener("click", () => {
-  console.log("helo");
+  console.log("toggle active state");
   shareButton.classList.toggle("active");
+});
+
+document.addEventListener("click", (event) => {
+  const isOutsideShareButton = !shareButton.contains(event.target);
+  const isOutsideShareOptions = !event.target.closest(".share-options");
+
+  if (isOutsideShareButton && isOutsideShareOptions) {
+    if (shareButton.classList.contains("active")) {
+      console.log("Dismissing popover");
+      shareButton.classList.remove("active");
+    }
+  }
 });
