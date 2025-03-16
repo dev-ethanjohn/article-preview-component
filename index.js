060650bd43df94083dd36c03e6e88c2a1ea2f053
EJohn <dev.ejohnpaguntalan@gmail.com>
2025-03-16 09:42:55 +0800

    implement dismissal of popover on outside click

--- index.js
+++ index.js
@@ -8,12 +8,14 @@ shareButton.addEventListener("click", () => {
 
 document.addEventListener("click", (event) => {
   const isOutsideShareButton = !shareButton.contains(event.target);
-  const isOutsideShareOptions = !event.target.closest(".share-options");
+  // const isOutsideShareOptions = !event.target.closest(".share-options");//NOTE: This also works but since it dynamically checks the ancestor, it might cause some performance issues
+  const isOutsideShareOptions = !shareOptions.contains(event.target);
+
+  // console.log(event.target);
+  // console.log("outside", isOutsideShareButton);
 
   if (isOutsideShareButton && isOutsideShareOptions) {
-    if (shareButton.classList.contains("active")) {
-      console.log("Dismissing popover");
-      shareButton.classList.remove("active");
-    }
+    console.log("Dismissing popover");
+    shareButton.classList.remove("active");
   }
 });
